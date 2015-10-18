f3d0fd2a208c091b2a572ccb143a65daf2f29543
A---- <A----@users.noreply.github.com>
2015-10-18 17:06:38 +0200

    Lenient file checking.
    Remove trailing whitespaces, check for unauthorized characters in
    filenames, ignores 0-length files.

--- actions/verify-file-presence.js
+++ actions/verify-file-presence.js
@@ -6,12 +6,21 @@ var async = require("async");
 module.exports = function (noop, callback) {
   var files = this.torrent.data.files;
 
-  async.reject(
+  var allGood = true;
+
+  async.filter(
     files,
     (function (item, callback) {
       var values = [
             item.path,
-            item.path.replace(/\\[ ]+/, '\\')
+            item.path
+              .replace(/\\[ ]+/g, '\\')               // Trumpable, a path shouldn't start with a space
+              .replace(/[ ]+\\/g, '\\')               // Trailing spaces are also a no-no on windows
+              .replace(/[\x00-\x1f?*:";‌​|/<>]+/g, ""), // Illegal characters on windows
+            item.path
+              .replace(/\\[ ]+/g, '\\_')
+              .replace(/[ ]+\\/g, '_\\')
+              .replace(/[\x00-\x1f?*:";‌​|/<>]+/g, "_")
           ],
           value;
 
@@ -25,14 +34,27 @@ module.exports = function (noop, callback) {
         }
       }
 
+      // If the file is 0-length and wasn't found, just ignore it.
+      if(item.length == 0) {
+        callback(false);
+        return;
+      }
+
+      allGood = false;
       callback(false);
     }).bind(this),
     (function (results){
-      if(results.length > 0) {
+      if(! allGood) {
         this.log.push(util.format("%s files not found out of %d", results.length, files.length));
+
+        for(var i = 0; i < results.length; i++) {
+          this.log.push(util.format("Not found: %s", results[i].path));
+        }
+
         callback("Can't find all files.".bold.red, null);
       }
       else {
+        this.torrent.data.files = results;
         this.log.push("All files".green + " accounted for");
         callback(null, null);
       }
